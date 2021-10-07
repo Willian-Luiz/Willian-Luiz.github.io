bf331636ccad5edabc74303e449bb7f7829d90c2
DinossauroBebado <guipsilva32@gmail.com>
2021-10-07 19:50:22 -0300

    class

--- JS/D&DExchange.js
+++ JS/D&DExchange.js
@@ -1,6 +1,10 @@
 
 
 function Conversao(valor,input,output){
+  console.log(valor)
+      console.log(input)
+      console.log(output)
+
   const conversão = [1,10,50,100,1000];
   // passa o valor para uma unidade padrão
   valor_base = valor/conversão[input-1];
@@ -17,7 +21,10 @@ function Calcular() {
   var index_moeda_in = document.getElementById("p1").selectedIndex
   var index_moeda_out = document.getElementById("p2").selectedIndex
 
-  valor_out = Conversao(valor_in,index_moeda_in,index_moeda_out);
-
-  document.getElementById("vemPraCa").value = valor_out;
+  var moedas = ["Cobre", "Prata", "Electrum", "Ouro", "Platina"]
+  console.log(moedas[index_moeda_in]);
+  //
+ valor_out = Conversao(valor_in,index_moeda_in,index_moeda_out);
+  //
+   document.getElementById("vemPraCa").value = valor_out;
 };
